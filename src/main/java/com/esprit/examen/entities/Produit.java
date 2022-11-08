8465171d9046049bcb02e0a50ee6ec5922062d9c
yosri.mtar <yosri.mtar@esprit.tn>
2022-11-09 00:46:25 +0100

    changed entity

--- src/main/java/com/esprit/examen/entities/Produit.java
+++ src/main/java/com/esprit/examen/entities/Produit.java
@@ -3,6 +3,7 @@ package com.esprit.examen.entities;
 import java.io.Serializable;
 import java.util.Date;
 import java.util.Set;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,11 +12,10 @@ import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+
+import lombok.Data;
 
 @Entity
 @Data
